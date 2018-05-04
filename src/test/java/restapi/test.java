8188db57a40c2bdca0e196c1aec6f82c001a9525
jgoloborodko <jevgeni@digital-magic.io>
2018-05-04 21:57:31 +0300

    Added backend test for search

--- src/test/java/restapi/test.java
+++ src/test/java/restapi/test.java
@@ -1,23 +1,33 @@
 package restapi;
 
 import beauty.LoginPage;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import common.GenericTest;
-import okhttp3.OkHttpClient;
+import okhttp3.*;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebDriver;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertEquals;
 
 public class test implements GenericTest {
     private static WebDriver driver;
     private static String URL;
-    private static Properties props = GenericTest.getProperties();
 
+	private static Properties props = GenericTest.getProperties();
     private static String username = props.getProperty("beauty.gmaillogin");
     private static String password = props.getProperty("beauty.gmailpassword");
+
+    private ObjectMapper objectMapper = new ObjectMapper();
 
     private final String baseUrl = "https://bo.digital-magic.io";
     private final OkHttpClient client = new OkHttpClient().newBuilder()
@@ -26,17 +36,60 @@ public class test implements GenericTest {
 
     @BeforeClass
     public static void SetUp(){
-        driver = GenericTest.getDriver();
+		Properties props = GenericTest.getProperties();
+
+		String login = props.getProperty("security.login");
+		String password = props.getProperty("security.password");
+		URL = "https://" + login + ":" + password  +"@bo.digital-magic.io";
     }
 
 
+    static class SearchRecord {
+    	public String id;
+    	public String name;
+	}
+
+    static class SearchResult {
+    	public List<SearchRecord> cities;
+    	public List<SearchRecord> districts;
+    	public List<SearchRecord> categories;
+    	public List<SearchRecord> services;
+    	public List<SearchRecord> resources;
+	}
+
+    @Test
+    public void search() throws IOException {
+
+    	String query = "hair";
+    	String url = baseUrl + "/api/search/suggestions/" + query;
+		Request request = new Request.Builder()
+				.get()
+				.url(url)
+				.build();
+
+		try (Response response = client.newCall(request).execute()) {
+			if (response.code() != 200) {
+				throw new IllegalArgumentException("Unexpected response status:" + response.code() + "; " + response.body().string());
+			}
+			String body = response.body().string();
+			System.out.println(body);
+			SearchResult result = objectMapper.readValue(body,SearchResult.class );
+			//assertEquals("Barber", result.categories.get(0).name);
+		}
+
+	}
+
     @Test
     public void testAuth() throws Exception {
+		driver = GenericTest.getDriver();
         driver.get(URL);
         driver.manage().window().maximize();
+		Thread.sleep(1000);
+		Set<Cookie> cookies = driver.manage().getCookies();
+		System.out.println("COOKIES: " + cookies.stream().map(c -> c.getName() + "=" + c.getValue()).collect(Collectors.joining("\n")));
         new LoginPage(driver).loginGmail(username, password);
-        Set<Cookie> cookies = driver.manage().getCookies();
-        System.out.println(cookies);
+        cookies = driver.manage().getCookies();
+		System.out.println("COOKIES: " + cookies.stream().map(c -> c.getName() + "=" + c.getValue()).collect(Collectors.joining("\n")));
         /*
         Request request = new Request.Builder()
             .get()
@@ -56,4 +109,13 @@ public class test implements GenericTest {
         }
         */
     }
+
+    @Test
+    public void testCookie() throws InterruptedException {
+		driver = GenericTest.getDriver();
+		driver.get("http://www.google.com");
+		Thread.sleep(1000);
+		Set<Cookie> cookies = driver.manage().getCookies();
+		System.out.println(cookies.stream().map(c -> c.getName() + "=" + c.getValue()).collect(Collectors.joining("\n")));
+	}
 }
